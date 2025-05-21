ea928e4cd6fae9a46dea138b8951c2f849885e3a
ShoaibHafeez4048 <141241426+ShoaibHafeez4048@users.noreply.github.com>
2025-05-21 19:38:17 +0530

    Pushing the developed code

---
+++ airflowFlightData_DAG.py
@@ -0,0 +1,53 @@
+from airflow import DAG
+from airflow.providers.google.cloud.operators.dataproc import DataprocCreateBatchOperator
+from airflow.models import Variable
+from airflow.providers.google.cloud.sensors.gcs import GCSObjectExistenceSensor
+
+my_DAG = DAG(
+    dag_id = 'FlightData_DAG',
+    default_args = {'retries': 1}
+)
+
+variable_dict = Variable.get("Env_vars")
+PROJECT_ID = variable_dict["PROJECT_ID"] # 'smart-arc-459310-g9'
+REGION = variable_dict["REGION"] # 'us-east1'
+BUCKET_NAME = variable_dict["BUCKET_NAME"]
+ENV_NAME = variable_dict["ENV_NAME"]
+PYSPARK_JOB = f'gs://{BUCKET_NAME}/spark-jobs/spark-job-flight.py'
+
+wait_for_gcs_file = GCSObjectExistenceSensor(
+    dag=my_DAG,
+    task_id='wait_for_gcs_file',
+    bucket=BUCKET_NAME,
+    object=f'data/source/{ENV_NAME}/flight_booking.csv',
+    poke_interval=30,
+    timeout=300,
+    mode='poke'    
+)
+
+BATCH = {
+    "pyspark_batch": {
+        "main_python_file_uri": PYSPARK_JOB
+    },
+    "runtime_config": {
+        "version": "2.1"
+    },
+    "environment_config": {
+        "execution_config": {
+            "service_account": "268628543886-compute@developer.gserviceaccount.com",
+            "network_uri": f"projects/{PROJECT_ID}/global/networks/default",
+            "subnetwork_uri": f"projects/{PROJECT_ID}/regions/{REGION}/subnetworks/default"
+        }
+    }
+}
+
+pyspark_task = DataprocCreateBatchOperator(
+    dag = my_DAG,
+    task_id="pyspark_task", 
+    batch = BATCH,
+    batch_id = 'dataproc_serverless',
+    region = REGION, 
+    project_id = PROJECT_ID
+)
+
+wait_for_gcs_file >> pyspark_task
---
+++ spark-job-flight.py
@@ -0,0 +1,22 @@
+from pyspark.sql import SparkSession
+from pyspark.sql import functions as f 
+from airflow.models import Variable
+
+spark = SparkSession.builder.appName('airflow1').getOrCreate()
+
+variable_dict = Variable.get("Env_vars")
+PROJECT_ID = variable_dict["PROJECT_ID"]
+BUCKET_NAME = variable_dict["BUCKET_NAME"]
+DATASET_NAME = variable_dict["DATASET_NAME"]
+TABLE_NAME = variable_dict["TABLE_NAME"]
+ENV_NAME = variable_dict["ENV_NAME"]
+
+# Reading the csv files
+flight_df = spark.read.format('csv').option('inferSchema', True).option('header', True).load(f'gs://{BUCKET_NAME}/data/source/{ENV_NAME}/flight_booking.csv')
+
+flight_df.write \
+    .format("bigquery") \
+    .option("table", f"{PROJECT_ID}.{DATASET_NAME}.{TABLE_NAME}") \
+    .option("writeMethod", "direct") \
+    .mode("overwrite") \
+    .save()
